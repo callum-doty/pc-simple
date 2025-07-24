95b05644de4208bddd4c207ddfba519906566948
callum-doty <doty.callum9@gmail.com>
2025-07-24 14:14:08 -0500

    rendering img

--- services/storage_service.py
+++ services/storage_service.py
@@ -71,6 +71,7 @@ class StorageService:
                 "s3",
                 aws_access_key_id=settings.s3_access_key,
                 aws_secret_access_key=settings.s3_secret_key,
+                region_name=settings.s3_region,
                 endpoint_url=endpoint_url,
                 config=Config(
                     signature_version="s3v4", s3={"addressing_style": "path"}
