c1c88fb787c0f6b2fd48a57a3956ec18eaddc2bc
callum-doty <doty.callum9@gmail.com>
2025-08-05 15:53:42 -0500

    versions issue

--- alembic/versions/9a9ed56149a0_enable_pgvector_extension.py
+++ alembic/versions/9a9ed56149a0_enable_pgvector_extension.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "9a9ed56149a0"
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = "0001"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
