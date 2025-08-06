91c57edbbfa811222b6ad0b7498fbf4a98d781bf
callum-doty <doty.callum9@gmail.com>
2025-08-06 15:24:38 -0500

    db reset

--- reset_database.sh
+++ reset_database.sh
@@ -4,6 +4,11 @@ set -e
 export RENDER=true
 
 echo "Resetting database..."
+
+# Clear storage directory
+echo "Clearing storage directory..."
+find dev_storage -mindepth 1 -delete
+
 alembic downgrade base
 alembic upgrade head
 echo "Database reset complete."
