3ce641244b74e692d6b73ca7e67cbfaa9ff3d52b
callum-doty <doty.callum9@gmail.com>
2025-08-07 15:52:03 -0500

    optim test 3

--- alembic/versions/a4fb095d8669_.py
+++ alembic/versions/a4fb095d8669_.py
@@ -1,8 +1,8 @@
 """empty message
 
 Revision ID: a4fb095d8669
-Revises: 65d45777a561, 76ce7437e49a
-Create Date: 2025-08-06 12:43:29.068601
+Revises: 43a3945b2fbe, 86a7849a0be1
+Create Date: 2025-08-07 15:50:54.040040
 
 """
 from typing import Sequence, Union
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a4fb095d8669'
-down_revision: Union[str, None] = ('65d45777a561', '76ce7437e49a')
+down_revision: Union[str, None] = ('43a3945b2fbe', '86a7849a0be1')
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
